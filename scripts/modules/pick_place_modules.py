f771af0bb52cfde34c9980a6c35125a1c3c9d420
asif <asif.rana@gatech.edu>
2016-12-16 11:02:42 -0500

    checking branch

--- scripts/modules/pick_place_modules.py
+++ scripts/modules/pick_place_modules.py
@@ -18,6 +18,7 @@ from utilities.kinect_pantilt import PanTilt
 
 class ObjectSearcher:
     def __init__(self, requestedObjectNameIn='BANANA_ARTIFICIAL'):
+        print "asif"
         rospy.wait_for_service('/rail_segmentation/segment')
         self.segmentationService = rospy.ServiceProxy('/rail_segmentation/segment', std_srvs.srv.Empty)
         self.recognitionSub = rospy.Subscriber("/object_recognition_listener/recognized_objects",
